7486769399ebb10a17f82d6ecf4d547a016e6eb7
ichigayamate <kyokodenshae353kei@gmail.com>
2025-04-11 18:17:06 +0700

    added next seo

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,6 +1,14 @@
 import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import type {AppProps} from "next/app";
+import {DefaultSeo} from "next-seo";
+import Header from "@/lib/components/headers";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+export default function App({Component, pageProps}: AppProps) {
+  return <>
+    <DefaultSeo titleTemplate="%s | FLIN" defaultTitle="FLIN"/>
+    <Header/>
+    <main className="p-4 mt-16 max-w-[1500px]">
+      <Component {...pageProps} />
+    </main>
+  </>;
 }
--- client/src/pages/_document.tsx
+++ client/src/pages/_document.tsx
@@ -1,15 +1,11 @@
 import {Html, Head, Main, NextScript} from "next/document";
-import Header from "@/lib/components/headers";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head/>
       <body className="antialiased">
-        <Header/>
-        <main className="p-4 mt-16 max-w-[1500px]">
-          <Main/>
-        </main>
+        <Main/>
         <NextScript/>
       </body>
     </Html>
